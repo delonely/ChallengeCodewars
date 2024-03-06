3aa267cad4334ee5a90504ed0bcd8810ba6938d7
Farid Azhari <farid@ganemid.com>
2024-03-06 08:44:35 +0700

    (update) simplify functions to one liner, add example how if we use callback

--- Day-10_FundamentalReturnProblem.js
+++ Day-10_FundamentalReturnProblem.js
@@ -28,33 +28,58 @@ Note: All math operations will be: a (operation) b
     assert.strictEqual(subt(1,2),-1)
 */
 
-function add(a,b){
-    return a + b;
-}
+// function add(a,b){
+//     return a + b;
+// }
 
-function divide(a,b){
-    return a / b;
-}
+// function divide(a,b){
+//     return a / b;
+// }
 
-function multiply(a,b){
-    return a * b;
-}
+// function multiply(a,b){
+//     return a * b;
+// }
 
-function mod(a,b){
-    return a % b;
-}
+// function mod(a,b){
+//     return a % b;
+// }
    
-function exponent(a,b){
-    return a ** b;
-}
+// function exponent(a,b){
+//     return a ** b;
+// }
     
-function subt(a,b){
-    return a - b;
-}
+// function subt(a,b){
+//     return a - b;
+// }
 
-console.log(add(1,2));
+// Solution for one liner
+let add = (a,b) => a + b;
+
+let divide = (a,b) => a / b;
+
+let multiply = (a,b) => a * b;
+
+let mod = (a,b) => a % b;
+   
+let exponent = (a,b) => a ** b;
+    
+let subt = (a,b) => a - b;
+
+// Trying solution with callback, not necessarily used
+let calculate = (a, b, callback) => callback(a, b);
+
+console.log(add(6,2));
 console.log(divide(6,2));
 console.log(multiply(6,2));
 console.log(mod(6,2));
 console.log(exponent(6,2));
 console.log(subt(6,2));
+
+let functions = [add, divide, multiply, mod, exponent, subt];
+functions.forEach((func) => console.log(calculate(6, 2, func)) );
+// console.log(calculate(1,2, add));
+// console.log(calculate(6,2, divide));
+// console.log(calculate(6,2, multiply));
+// console.log(calculate(6,2, mod));
+// console.log(calculate(6,2, exponent));
+// console.log(calculate(6,2, subt));
