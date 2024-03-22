cfa04a276e1f40b1422ec98781835ce61b20c3c8
Farid Azhari <farid@ganemid.com>
2024-03-22 20:35:34 +0700

    (update) added 3rd solution using map

--- Day-16_InvertValuesProblem.js
+++ Day-16_InvertValuesProblem.js
@@ -32,21 +32,24 @@ assert.deepEqual(norm(invert([0])), [0]);
 // }
 
 //SOLUTION 2
-function invert(array) {
-    let invertArray = [];
-    for(let i = 0; i < array.length; i++){
-       if(array[i] > 0){
-        invertArray.push(array[i] - (array[i] *2));
-       }
-       if (array[i] < 0){
-        invertArray.push(Math.abs(array[i]));
-       }  
-       if (array[i] == 0) {
-        invertArray.push(-0);
-       }
-    }  
-    return invertArray;
-}
+// function invert(array) {
+//     let invertArray = [];
+//     for(let i = 0; i < array.length; i++){
+//        if(array[i] > 0){
+//         invertArray.push(array[i] - (array[i] *2));
+//        }
+//        if (array[i] < 0){
+//         invertArray.push(Math.abs(array[i]));
+//        }  
+//        if (array[i] == 0) {
+//         invertArray.push(-0);
+//        }
+//     }  
+//     return invertArray;
+// }
 
+// Solution 3
+let invert = (array) => array.map((value) => value == 0 ? 0 : value * -1);
  
- console.log(invert([1,2,3,4,5]));
+// console.log(invert([1,2,3,4,5]));
+console.log(invert([0]));
