f50ccb1aae113db915912e6fe11bfd344bf7a33d
Farid Azhari <farid@ganemid.com>
2024-04-16 09:02:52 +0700

    (update) use array reduce for simpler process

--- Day-24_WellOfIdeas.js
+++ Day-24_WellOfIdeas.js
@@ -13,20 +13,30 @@ In this kata you need to check the provided array (x) for good ideas 'good' and 
     assert.strictEqual(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']), 'I smell a series!');
  */
 
-function well(x) {
-  let idea = 0;
-  for (let i = 0; i < x.length; i++) {
-    if (x[i] === "good") {
-      idea += 1;
-    }
-  }
-  if (idea === 1 || idea === 2) {
-    return "Publish!";
-  } else if (idea >= 1) {
-    return "I smell a series!";
-  } else {
-    return "Fail!";
-  }
-}
+// function well(x) {
+//   let idea = 0;
+//   for (let i = 0; i < x.length; i++) {
+//     if (x[i] === "good") {
+//       idea += 1;
+//     }
+//   }
+//   if (idea === 1 || idea === 2) {
+//     return "Publish!";
+//   } else if (idea >= 1) {
+//     return "I smell a series!";
+//   } else {
+//     return "Fail!";
+//   }
+// }
 
+const well = (x) => {
+  // Use total + boolean since boolean return 1 for true and 0 for false
+  let idea = x.reduce((total, currentValue) => total + (currentValue === "good"), 0);
+
+  // Without using else, since return would break function and won't continue to next line
+  if (idea <= 0) return "Fail!";
+  if (idea <= 2) return "Publish!";
+  return "I smell a series!";
+
+}
 console.log(well(["bad", "good", "good", "bad"]));
