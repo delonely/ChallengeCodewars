15c19f3f090159c6b955c20407b1b370c65e2aa4
delonely <andirezza26@gmail.com>
2024-11-04 08:26:10 +0700

    Day-32 Problem

---
+++ Day-32_IsHeGonnaSurviveProblem.js
@@ -0,0 +1,33 @@
+/**
+ * 
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return true if yes, false otherwise :)
+
+TEST CASE
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe('Fixed tests', function() {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(hero(10, 5), true);
+    assert.strictEqual(hero(7, 4), false);
+    assert.strictEqual(hero(4, 5), false);
+    assert.strictEqual(hero(100, 40), true);
+    assert.strictEqual(hero(1500, 751), false);
+    assert.strictEqual(hero(0, 1), false);
+  });
+});
+ */
+
+function hero(bullets, dragons){
+    //Get Coding!
+    return bullets >= (2 * dragons)
+}
+console.log(hero(10,5))
+console.log(hero(7,4))
+console.log(hero(4,5))
+console.log(hero(100,40))
+console.log(hero(1500,751))
+console.log(hero(0,1))
