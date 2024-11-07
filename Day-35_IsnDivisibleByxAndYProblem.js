cc3b31a2c1265ccf17ef4279a3253ba616a1e5a8
Farid Azhari <farid@ganemid.com>
2024-11-07 10:46:57 +0700

    (update) change to oneliner, if n % x = 0 and n% y =0, then n % (x*y) = 0

--- Day-35_IsnDivisibleByxAndYProblem.js
+++ Day-35_IsnDivisibleByxAndYProblem.js
@@ -22,11 +22,13 @@ describe("Basic tests", () => {
 });
  */
 
-function isDivisible(n, x, y) {
-    if (n % x === 0 && n % y === 0){
-        return true;
-    } else return false;
-}
+// function isDivisible(n, x, y) {
+//     if (n % x === 0 && n % y === 0){
+//         return true;
+//     } else return false;
+// }
+
+let isDivisible = (n, x, y) => n % (x * y) == 0;
 
 console.log(isDivisible(3,3,4))
 console.log(isDivisible(12,3,4))
