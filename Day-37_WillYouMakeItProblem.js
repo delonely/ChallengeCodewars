7897aab6ae1087024b0dbaf70203663373190784
Farid Azhari <farid@ganemid.com>
2024-11-11 10:02:48 +0700

    (update) change to really one liner

--- Day-37_WillYouMakeItProblem.js
+++ Day-37_WillYouMakeItProblem.js
@@ -16,9 +16,7 @@ describe("zeroFill", function() {
 });
  */
 
-const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-    return fuelLeft * mpg >= distanceToPump;
-};
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => fuelLeft * mpg >= distanceToPump
 
 console.log(zeroFuel(50, 25, 2))
 console.log(zeroFuel(100, 50, 1))
