34bd558d94c3fbafe786a7dd5bce508d1b2b489d
delonely <andirezza26@gmail.com>
2024-11-18 11:58:08 +0700

    Day-40 Problem

---
+++ Day-40_VowelRemoverProblem.js
@@ -0,0 +1,36 @@
+/**
+ * Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+Examples
+
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+
+    don't worry about uppercase vowels
+    y is not considered a vowel for this kata
+
+TEST CASE
+const strictEqual = require('chai').assert.strictEqual;
+
+function doTest (input, expected) {
+  const actual = shortcut(input);
+  strictEqual(actual, expected, `for "${input}":\n`);
+}
+
+describe("Sample Tests", () => {
+  it("Should pass sample tests", () => {
+    doTest('hello', 'hll');
+    doTest('how are you today?', 'hw r y tdy?');
+    doTest('complain', 'cmpln');
+    doTest('never', 'nvr');
+  });    
+});
+ */
+
+function shortcut (string) {
+    const removedVowel = string.replace(/[aiueo]/gi, "");
+    return removedVowel;
+}
+
+console.log(string('hello'));
